abfafe360fdfab9956a540e5d81ab4a5eeefd51d
snowylchen <491429856@qq.com>
2022-01-08 17:05:36 +0800

    数字回显

--- src/main/java/com/cj/cptrend/colorseed/controller/ColorSeedController.java
+++ src/main/java/com/cj/cptrend/colorseed/controller/ColorSeedController.java
@@ -8,8 +8,10 @@ import com.cj.cptrend.utils.HttpUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -28,26 +30,42 @@ public class ColorSeedController {
         return PREFIX + "colorseed";
     }
 
-    public static void main(String[] args) {
-        String data = HttpUtils.doPost("https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=100&isVerify=1&pageNo=1", new StringBuilder());
+
+    @RequestMapping("/getData")
+    @ResponseBody
+    public Integer insertColorSeed(ModelMap modelMap) {
+        String data = HttpUtils.doGet("https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=10000&isVerify=1&pageNo=1", null);
         JSONObject jsonObject = JSON.parseObject(data);
         String string = JSON.parseObject(jsonObject.getString("value")).getString("list");
         List<JSONObject> jsonObjects = JSON.parseArray(string, JSONObject.class);
+        List<Lottery> lotteries = new ArrayList<>();
         for (JSONObject object : jsonObjects) {
-            Lottery lottery = new Lottery();
-            String lotteryDrawNum = object.getString("lotteryDrawNum");
-            lottery.setNumPeriods(Integer.valueOf(lotteryDrawNum));
-            String pre = object.getString("lotteryUnsortDrawresult").split(" ")[0];
-            String suf = object.getString("lotteryUnsortDrawresult").split(" ")[1];
-            String[] preNum = pre.split("\\+");
-            lottery.setPreNum1(preNum[0]);
-            lottery.setPreNum2(preNum[1]);
-            lottery.setPreNum3(preNum[2]);
-            lottery.setPreNum4(preNum[3]);
-            lottery.setPreNum5(preNum[4]);
-            String[] sufNum = suf.split("\\+");
-            lottery.setSufNum1(sufNum[0]);
-            lottery.setSufNum2(sufNum[1]);
+            try {
+
+                Lottery lottery = new Lottery();
+                String lotteryDrawNum = object.getString("lotteryDrawNum");
+                lottery.setNumPeriods(Integer.valueOf(lotteryDrawNum));
+//                String pre = object.getString("lotteryUnsortDrawresult").split(" ")[0];
+//                String suf = object.getString("lotteryUnsortDrawresult").split(" ")[1];
+                String[] d = object.getString("lotteryDrawResult").split(" ");
+                lottery.setPreNum1(d[0]);
+                lottery.setPreNum2(d[1]);
+                lottery.setPreNum3(d[2]);
+                lottery.setPreNum4(d[3]);
+                lottery.setPreNum5(d[4]);
+                lottery.setSufNum1(d[5]);
+                lottery.setSufNum2(d[6]);
+                lottery.setDate(object.getString("lotteryDrawTime"));
+                lotteries.add(lottery);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        lotteries.parallelStream().forEach(item -> {
+            lotteryService.insertLottery(item);
+        });
+        return 1;
     }
+
+
 }
--- src/main/java/com/cj/cptrend/utils/HttpUtils.java
+++ src/main/java/com/cj/cptrend/utils/HttpUtils.java
@@ -4,6 +4,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -60,4 +61,48 @@ public class HttpUtils {
         }
         return null;
     }
+
+    public static String doGet(String url, StringBuilder params) {
+        // 获得Http客户端
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Post请求
+        HttpGet httpGet = new HttpGet(url + (params == null ? "" : "?" + params));
+        // 设置ContentType
+//        HttpGet.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        // 响应模型
+        CloseableHttpResponse response = null;
+
+        try {
+            // 由客户端执行(发送)Post请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                //返回数据
+                String responseString = EntityUtils.toString(responseEntity);
+                return responseString;
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
 }
