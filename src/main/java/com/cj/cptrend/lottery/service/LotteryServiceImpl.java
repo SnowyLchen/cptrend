8b0ffe31e8b75900fa2116cc2b79774a20753988
snowylchen <491429856@qq.com>
2022-01-11 08:33:13 +0800

    数字回显

--- src/main/java/com/cj/cptrend/lottery/service/LotteryServiceImpl.java
+++ src/main/java/com/cj/cptrend/lottery/service/LotteryServiceImpl.java
@@ -27,7 +27,7 @@ public class LotteryServiceImpl implements ILotteryService {
 
     @Override
     public List<Lottery> selectLotteryList(Lottery lottery) {
-        return mLotteryReadMapper.selectLotteryList(lottery);
+        return mLotteryReadMapper.selectLotteryList(lottery).subList(0, 10);
     }
 
     @Override
